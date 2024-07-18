c40da8455dbd8d60d2bffa4580861a05375899e8
kimsungyeop <skuksyzkzk@gmail.com>
2024-07-18 15:47:58 +0900

    feat: 강의 시작
    토비의 스프링부트 강의 시작

---
+++ src/main/java/com/helloboot/tobispringboot/TobiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.helloboot.tobispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobiSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/helloboot/tobispringboot/TobiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloboot.tobispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
