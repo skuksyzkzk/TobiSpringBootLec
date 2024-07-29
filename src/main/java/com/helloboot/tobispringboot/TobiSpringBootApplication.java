a02f2285e7ef1fd19cd389314b1847161cc5dac0
kimsungyeop <skuksyzkzk@gmail.com>
2024-07-29 23:19:24 +0900

    feat: servlet

--- src/main/java/com/helloboot/tobispringboot/TobiSpringBootApplication.java
+++ src/main/java/com/helloboot/tobispringboot/TobiSpringBootApplication.java
@@ -1,13 +1,30 @@
 package com.helloboot.tobispringboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 
-@SpringBootApplication
+import java.io.IOException;
+
 public class TobiSpringBootApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(TobiSpringBootApplication.class, args);
-    }
+        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setStatus(200);
+                    resp.setHeader("Content-Type","text/plain");
+                    resp.getWriter().println("Hello Servlet");
+                }
+            }).addMapping("/hello");
 
+        });
+        webServer.start();
+    }
 }
