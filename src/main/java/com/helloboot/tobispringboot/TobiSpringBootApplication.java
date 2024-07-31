7ca4a23957c58a1e4f8ec18ec70c5a1a82da819f
kimsungyeop <skuksyzkzk@gmail.com>
2024-07-31 18:10:50 +0900

    feat: 독립 실행형 스프링 컨테이너 사용

--- src/main/java/com/helloboot/tobispringboot/TobiSpringBootApplication.java
+++ src/main/java/com/helloboot/tobispringboot/TobiSpringBootApplication.java
@@ -8,21 +8,40 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import java.io.IOException;
 
 public class TobiSpringBootApplication {
     public static void main(String[] args) {
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(TobiController.class); // 빈 등록
+        applicationContext.refresh(); // 여기서 빈 오브젝트 만들어짐 
+        
         TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
         WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+            servletContext.addServlet("frontController", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    resp.setStatus(200);
-                    resp.setHeader("Content-Type","text/plain");
-                    resp.getWriter().println("Hello Servlet");
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
+
+                        TobiController helloController = applicationContext.getBean(TobiController.class);
+                        String ret = helloController.hello(name);
+
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println(ret);
+                    }
+                    else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
+
                 }
-            }).addMapping("/hello");
+            }).addMapping("/*");
 
         });
         webServer.start();
